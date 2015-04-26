b83bbc15e9efcc43958c0d27c764a84f1fd712fc
Christian <dude_@gmx.net>
2015-04-26 14:08:34 +0200

    run

---
+++ run_analysis.R
@@ -0,0 +1,80 @@
+
+# setting working directory
+setwd("D:/Schulungen/coursera/DataScience-Specialization/R-Developing/3_GettingCleaningData/CourseProject/UCI_HAR_Dataset")
+
+# load the needed data
+TestX<- read.table("./test/X_test.txt")
+TestY <- read.table("./test/y_test.txt")
+TestSub <- read.table("./test/subject_test.txt")
+
+TrainX<- read.table("./train/X_train.txt")
+TrainY <- read.table("./train/y_train.txt")
+TrainSub <- read.table("./train/subject_train.txt")
+
+features <- read.table("./features.txt")
+activity_labels <- read.table("./activity_labels.txt")
+
+# setting column names
+
+colnames(TestX) <- features[,2]
+colnames(TrainX) <- features[,2]
+colnames(TestSub) <- "Subject_ID"
+colnames(TrainSub) <- "Subject_ID"
+colnames(TestY) <- "Act_ID"
+colnames(TrainY) <- "Act_ID"
+
+# 1. Merges the training and the test sets to create one data set.
+
+total_Test <- cbind(TestSub, TestY, TestX) 
+total_Train <- cbind(TrainSub, TrainY, TrainX) 
+total <- rbind(total_Test, total_Train)
+
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+columnNames <- colnames(total)
+
+MeanStdCols <- grepl("(Subject_ID|Act_ID|mean|std", colNames,ignore.case=T)    # Build a logical vector with TRUE for the needed columns
+
+total_relevant <- total[MeanStdCols==TRUE]    # Discard every column not needed
+
+
+# 3. Uses descriptive activity names to name the activities in the data set
+
+colnames(activity_labels) <- c("Act_ID", "Activity Category")
+
+total_act <- merge(activity_labels,total_relevant,by.x="Act_ID",by.y="Act_ID",all=TRUE)
+
+
+columnNames <- colnames(total_act)   # include "Activity Category" in colNames
+
+
+
+# 4. Appropriately labels the data set with descriptive variable names. 
+
+
+# Cleaning up with following clearing rules
+for (i in 1:length(columnNames)) 
+{
+  columnNames[i] = gsub("-mean","Mean",columnNames[i]) 
+  
+  columnNames[i] = gsub("-std$","Standard Deviation",columnNames[i])
+  columnNames[i] = gsub("\\()","",columnNames[i])
+  columnNames[i] = gsub("^(t)","time",columnNames[i])
+  columnNames[i] = gsub("^(f)","freq",columnNames[i])
+  columnNames[i] = gsub("BodyBody","Body",columnNames[i])
+}
+
+colnames(total_act) = columnNames   # set cleared column Names 
+
+
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+totalminusactID  = total_act[,names(total_act) != 'Act_ID']  # eleminate "ACT_ID"
+
+
+# creating tidy data set with the means for each variable for each activity and each subject
+tidyData <- aggregate(totalminusactID[,names(totalminusactID) != c("Activity Category","Subject_ID")],by=list("Activity Category"=totalminusactID$"Activity Category",Subject_ID = totalminusactID$Subject_ID),mean)
+
+
+
+write.table(tidyData, './tidyData.txt',row.names=FALSE,sep='\t') # Export the tidy data set 
